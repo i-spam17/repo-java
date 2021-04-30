e8d876cc382a13c7fd6e11825b3dedaa78bbd7e5
i-spam <i_spam@bk.ru>
2021-04-30 19:59:51 +0500

    commit lesson1 & lesson2

---
+++ src/main/java/ru/gb/java1/lesson1/Lesson1.java
@@ -0,0 +1,13 @@
+package ru.gb.java1.lesson1;
+
+public class Lesson1 {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("************");
+        System.out.println("1234567890");
+        System.out.println("************");
+        System.out.println("Превед, Медвед!");
+
+    }
+}
---
+++ src/main/java/ru/gb/java1/lesson2/lesson2.java
@@ -0,0 +1,5 @@
+package ru.gb.java1.lesson2;
+
+public class lesson2 {
+
+}
