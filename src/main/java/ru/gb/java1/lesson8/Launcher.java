5fe9d3dc7647deb8fd8d73f6d5b58dd4bf629870
i-spam <i_spam@bk.ru>
2021-05-27 21:37:46 +0500

    commit homeWork8

---
+++ src/main/java/ru/gb/java1/lesson8/Launcher.java
@@ -0,0 +1,16 @@
+package ru.gb.java1.lesson8;
+
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 17.05.2021
+ */
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        MainWindow mainWindow = new MainWindow();
+
+    }
+
+}
